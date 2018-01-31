169c386e38567c2dacd1bcf9c53663def75011dc
Lukas Klingsbo <lukas.klingsbo@klarna.com>
2018-01-31 14:10:13 +0100

    Added crytocurrency coin price price lookup script

---
+++ altcoin.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+printf "$1: " &&
+curl -s https://min-api.cryptocompare.com/data/price\?fsym\=$1\&tsyms\=EUR | python2 -c "import json, sys; alt=json.load(sys.stdin); print str(alt['EUR'])"
