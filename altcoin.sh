c08aad07c11dea68250519f8cf11156df95a3769
Lukas Klingsbo <lukas.klingsbo@klarna.com>
2018-04-27 17:24:59 +0200

    Check euro instead of usd

--- altcoin.sh
+++ altcoin.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 printf "$1: " &&
-curl -s https://min-api.cryptocompare.com/data/price\?fsym\=$1\&tsyms\=USD |
-python2 -c "import json, sys; alt=json.load(sys.stdin); print str(alt['USD'])" ||
+curl -s https://min-api.cryptocompare.com/data/price\?fsym\=$1\&tsyms\=EUR |
+python2 -c "import json, sys; alt=json.load(sys.stdin); print str(alt['EUR'])" ||
 echo "No connection"
