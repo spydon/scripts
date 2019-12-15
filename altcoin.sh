74eac858742e706fe9b083175de520b99772b448
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2019-12-15 15:56:55 +0100

    Altcoin no connection = -

--- altcoin.sh
+++ altcoin.sh
@@ -2,4 +2,4 @@
 printf "$1: " &&
 curl -s https://min-api.cryptocompare.com/data/price\?fsym\=$1\&tsyms\=EUR |
 python2 -c "import json, sys; alt=json.load(sys.stdin); print str(alt['EUR'])" ||
-echo "No connection"
+echo "-"
